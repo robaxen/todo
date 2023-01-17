b6bb3e7fccad0fe892a2369020ef233a2cbb8961
robaxen <robaxen.huyy@gmail.com>
2023-01-17 09:19:49 +0200

    skapat xml filen och gjort lite ändringar i tabellen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.buttonNewNote = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDone = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,20 +45,52 @@
             this.buttonNewNote.TabIndex = 0;
             this.buttonNewNote.Text = "Ny note";
             this.buttonNewNote.UseVisualStyleBackColor = true;
+            this.buttonNewNote.Click += new System.EventHandler(this.buttonNewNote_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 41);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(402, 227);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(615, 82);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(75, 23);
+            this.buttonColor.TabIndex = 2;
+            this.buttonColor.Text = "Ändra färg";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(258, 274);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 3;
+            this.buttonEdit.Text = "Redigera";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonDone
+            // 
+            this.buttonDone.Location = new System.Drawing.Point(339, 274);
+            this.buttonDone.Name = "buttonDone";
+            this.buttonDone.Size = new System.Drawing.Size(75, 23);
+            this.buttonDone.TabIndex = 4;
+            this.buttonDone.Text = "Klart";
+            this.buttonDone.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(802, 436);
+            this.Controls.Add(this.buttonDone);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonColor);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonNewNote);
             this.Name = "Form1";
@@ -68,6 +104,10 @@
 
         private System.Windows.Forms.Button buttonNewNote;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDone;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Windows.Forms;
 
 //notes för användning av github och visual studio
@@ -32,8 +33,6 @@ namespace todo
             table1.Columns.Add("deadline");
             table1.Columns.Add("color");
 
-
-
             //lägger till en test rad i tabellen
             table1.Rows.Add("1");
             table1.Rows.Add("yrkesprov ");
@@ -41,12 +40,18 @@ namespace todo
             table1.Rows.Add("mars");
             table1.Rows.Add("#222");
 
-            ////skapar ett dataset och lägger dit tabellen
-            //DataSet set = new DataSet("tabeller");
-            //set.Tables.Add(table1);
+            //skapar ett dataset och lägger dit tabellen
+            DataSet ds = new DataSet("tabeller");
+            ds.Tables.Add(table1);
 
-            ////skriver ut dataset i console, skriver ut notes tabellens innehåll
-            //Console.WriteLine(set.GetXml());
+            //skriver ut dataset i console, skriver all innehåll från dataset ds
+            Console.WriteLine(ds.GetXml());
+
+            //tabellens innehåll sparas i notes.xml, som hamnar i debug mappen
+            table1.WriteXml("notes.xml");
+
+            
+
 
         }
 
@@ -57,6 +62,23 @@ namespace todo
             using (DataSet set = new DataSet("tabeller"))
             {
 
+            }
+        }
+
+        private void buttonNewNote_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //knapp som öppnar färg ruta, (flytta till lämpligare ställe senare)
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            //här öppnas det upp ett fönster där man kan välja en färg, colorDialog1 används för det
+            //ändrar färg bara om man väljer "ok"
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //(ändra "buttonColor" till note)
+                buttonColor.BackColor = colorDialog1.Color;
             }
         }
     }
